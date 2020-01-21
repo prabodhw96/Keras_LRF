bafdcbfce9980cb004d77aeb4572fc124a629330
prabodhw96 <prabodhwankhede1996@gmail.com>
2020-01-21 12:26:53 +0530

    fix typo

--- learningratefinder.py
+++ learningratefinder.py
@@ -22,128 +22,128 @@ class LearningRateFinder:
         self.batchNum = 0 # The current batch number update
         self.weightsFile = None # Our path to the initial model weights
         
-        def reset(self):
-            self.lrs = []
-            self.losses = []
-            self.lrMult = 1
-            self.avgLoss = 0
-            self.bestLoss = 1e9
-            self.batchNum = 0
-            self.weightsFile = None
+    def reset(self):
+        self.lrs = []
+        self.losses = []
+        self.lrMult = 1
+        self.avgLoss = 0
+        self.bestLoss = 1e9
+        self.batchNum = 0
+        self.weightsFile = None
             
-        def is_data_iter(self, data):
-            # Define the set of class types we will check for
-            iterClasses = ["NumpyArrayIterator", "DirectoryIterator", "DataFrameIterator", "Iterator", "Sequence"]
-            # Return whether our data is an iterator
-            return data.__class__.__name__ in iterClasses
+    def is_data_iter(self, data):
+        # Define the set of class types we will check for
+        iterClasses = ["NumpyArrayIterator", "DirectoryIterator", "DataFrameIterator", "Iterator", "Sequence"]
+        # Return whether our data is an iterator
+        return data.__class__.__name__ in iterClasses
         
-        def on_batch_end(self, batch, logs):
-            # Grab the current learning rate and log it to the list of learning rates that we've tried
-            lr = K.get_value(self.model.optimizer.lr)
-            self.lrs.append(lr)
-            # Grab the loss at the end of this batch,
-            # increment the total number of batches processed,
-            # smooth it, and update the losses list with the smoothed value
-            l = logs["loss"]
-            self.batchNum += 1
-            self.avgLoss = (self.beta * self.avgLoss) + ((1 - self.beta) * l)
-            smooth = self.avgLoss / (1 - (self.beta ** self.batchNum))
-            self.losses.append(smooth)
-            
-            # Compute the maximum loss stopping factor value
-            stopLoss = self.stopFactor * self.bestLoss
-            
-            # Check to see whether the loss has grown too large
-            if self.batchNum > 1 and smooth > stopLoss:
-                # Stop returning and return from the method
-                self.model.stop_training = True
-                return
-            
-            # Check to see if the best loss should be updated
-            if self.batchNum == 1 or smooth < self.bestLoss:
-                self.bestLoss = smooth
-                
-            # Increase the learning rate
-            lr *= self.lrMult
-            K.set_value(self.model.optimizer.lr, lr)
+    def on_batch_end(self, batch, logs):
+        # Grab the current learning rate and log it to the list of learning rates that we've tried
+        lr = K.get_value(self.model.optimizer.lr)
+        self.lrs.append(lr)
+        # Grab the loss at the end of this batch,
+        # increment the total number of batches processed,
+        # smooth it, and update the losses list with the smoothed value
+        l = logs["loss"]
+        self.batchNum += 1
+        self.avgLoss = (self.beta * self.avgLoss) + ((1 - self.beta) * l)
+        smooth = self.avgLoss / (1 - (self.beta ** self.batchNum))
+        self.losses.append(smooth)
         
-        def find(self, trainData, startLR, endLR, epochs=None, stepsPerEpoch=None, batchSize=32, sampleSize=2048, verbose=1):
-            # Reset our class specific variables
-            self.reset()
+        # Compute the maximum loss stopping factor value
+        stopLoss = self.stopFactor * self.bestLoss
+        
+        # Check to see whether the loss has grown too large
+        if self.batchNum > 1 and smooth > stopLoss:
+            # Stop returning and return from the method
+            self.model.stop_training = True
+            return
+        
+        # Check to see if the best loss should be updated
+        if self.batchNum == 1 or smooth < self.bestLoss:
+            self.bestLoss = smooth
             
-            # Determine if we are using a data generator or not
-            useGen = self.is_data_iter(trainData)
+        # Increase the learning rate
+        lr *= self.lrMult
+        K.set_value(self.model.optimizer.lr, lr)
+        
+    def find(self, trainData, startLR, endLR, epochs=None, stepsPerEpoch=None, batchSize=32, sampleSize=2048, verbose=1):
+        # Reset our class specific variables
+        self.reset()
+        
+        # Determine if we are using a data generator or not
+        useGen = self.is_data_iter(trainData)
+        
+        # If we're using a generator and steps per epoch is not supplied, raise an error
+        if useGen and stepsPerEpoch is None:
+            msg = "Using generator without supplying stepsPerEpoch"
+            raise Exception(msg)
             
-            # If we're using a generator and steps per epoch is not supplied, raise an error
-            if useGen and stepsPerEpoch is None:
-                msg = "Using generator without supplying stepsPerEpoch"
-                raise Exception(msg)
-                
-            # If we're not using a generator then our entire dataset must already be in the memory
-            elif not useGen:
-                # Grab the number of samples in the training data and then derive the number of steps per epoch
-                numSamples = len(trainData[0])
-                stepsPerEpoch = np.ceil(numSamples / float(batchSize))
-                
-            # If no number of training epochs are supplied,
-            # compute the training epochs based on a default sample size
-            if epochs is None:
-                epochs = int(np.ceil(numSamples / float(batchSize)))
-                
-            # Compute the total number of batch updates that will take place
-            # while we are attempting to find a good starting learning rate
-            numBatchUpdates = epochs * stepsPerEpoch
+        # If we're not using a generator then our entire dataset must already be in the memory
+        elif not useGen:
+            # Grab the number of samples in the training data and then derive the number of steps per epoch
+            numSamples = len(trainData[0])
+            stepsPerEpoch = np.ceil(numSamples / float(batchSize))
             
-            # Derive the learning rate multiplier based on ending learning rate, starting learning rate, and total number of batch updates
-            self.lrMult = (endLR / startLR) ** (1.0 / numBatchUpdates)
+        # If no number of training epochs are supplied,
+        # compute the training epochs based on a default sample size
+        if epochs is None:
+            epochs = int(np.ceil(numSamples / float(batchSize)))
             
-            # Create a temporary file path for the model weights and then save the weights
-            # (so we can reset the weights when we are done)
-            self.weightsFile = tempfile.mkstemp()[1]
-            self.model.save_weights(self.weightsFile)
+        # Compute the total number of batch updates that will take place
+        # while we are attempting to find a good starting learning rate
+        numBatchUpdates = epochs * stepsPerEpoch
+        
+        # Derive the learning rate multiplier based on ending learning rate, starting learning rate, and total number of batch updates
+        self.lrMult = (endLR / startLR) ** (1.0 / numBatchUpdates)
+        
+        # Create a temporary file path for the model weights and then save the weights
+        # (so we can reset the weights when we are done)
+        self.weightsFile = tempfile.mkstemp()[1]
+        self.model.save_weights(self.weightsFile)
+        
+        # Grab the *original* learning rate (so we can reset it later), and then set the *starting* learning rate
+        origLR = K.get_value(self.model.optimizer.lr)
+        K.set_value(self.model.optimizer.lr, startLR)
+        
+        # Construct a callback that will be called at the end of each batch,
+        # enabling us to increase our learning rate as training progresses
+        callback = LambdaCallback(on_batch_end=lambda batch, logs: self.on_batch_end(batch, logs))
+        
+        # Check to see if we are using a data iterator
+        if useGen:
+            self.model.fit_generator(
+                trainData,
+                steps_per_epoch=stepsPerEpoch,
+                epochs=epochs,
+                verbose=verbose,
+                callbacks=[callback])
+        
+        # Otherwise, our entire training data is already in memory
+        else:
+            # Train our model using Keras' fit method
+            self.model.fit(
+                trainData[0], trainData[1],
+                batch_size=batchSize,
+                epochs=epochs,
+                verbose=verbose,
+                callbacks=[callback])
             
-            # Grab the *original* learning rate (so we can reset it later), and then set the *starting* learning rate
-            origLR = K.get_value(self.model.optimizer.lr)
-            K.set_value(self.model.optimizer.lr, startLR)
+        # Restore the original model weights and learning rate
+        self.model.load_weights(self.weightsFile)
+        K.set_value(self.model.optimizer.lr, origLR)
             
-            # Construct a callback that will be called at the end of each batch,
-            # enabling us to increase our learning rate as training progresses
-            callback = LambdaCallback(on_batch_end=lambda batch, logs: self.on_batch_end(batch, logs))
-            
-            # Check to see if we are using a data iterator
-            if useGen:
-                self.model.fit_generator(
-                    trainData,
-                    steps_per_epoch=stepsPerEpoch,
-                    epochs=epochs,
-                    verbose=verbose,
-                    callbacks=[callback])
-            
-            # Otherwise, our entire training data is already in memory
-            else:
-                # Train our model using Keras' fit method
-                self.model.fit(
-                    trainData[0], trainData[1],
-                    batch_size=batchSize,
-                    epochs=epochs,
-                    verbose=verbose,
-                    callbacks=[callback])
-                
-            # Restore the original model weights and learning rate
-            self.model.load_weights(self.weightsFile)
-            K.set_value(self.model.optimizer.lr, origLR)
-            
-        def plot_loss(self, skipBegin=10, skipEnd=1, title=""):
-            # Grab the learning rate and losses values to plot
-            lrs = self.lrs[skipBegin:-skipEnd]
-            losses = self.losses[skipBegin:-skipEnd]
-            
-            # Plot the learning rate vs. loss
-            plt.plot(lrs, losses)
-            plt.xscale("log")
-            plt.xlabel("Learning Rate (Log Scale)")
-            plt.ylabel("Loss")
-            
-            # If the title is not empty, add it to the plot
-            if title != "":
-                plt.title(title)
+    def plot_loss(self, skipBegin=10, skipEnd=1, title=""):
+        # Grab the learning rate and losses values to plot
+        lrs = self.lrs[skipBegin:-skipEnd]
+        losses = self.losses[skipBegin:-skipEnd]
+        
+        # Plot the learning rate vs. loss
+        plt.plot(lrs, losses)
+        plt.xscale("log")
+        plt.xlabel("Learning Rate (Log Scale)")
+        plt.ylabel("Loss")
+        
+        # If the title is not empty, add it to the plot
+        if title != "":
+            plt.title(title)
--- minigooglenet.py
+++ minigooglenet.py
@@ -19,8 +19,8 @@ class MiniGoogLeNet:
 	@staticmethod
 	def inception_module(x, numK1x1, numK3x3, chanDim):
 		# define two CONV modules, then concatenate across the channel dimension
-		conv_1x1 = MiniGoogleNet.conv_module(x, numK1x1, 1, 1, (1, 1), chanDim)
-		conv_3x3 = MiniGoogleNet.conv_module(x, numK3x3, 3, 3, (1, 1), chanDim)
+		conv_1x1 = MiniGoogLeNet.conv_module(x, numK1x1, 1, 1, (1, 1), chanDim)
+		conv_3x3 = MiniGoogLeNet.conv_module(x, numK3x3, 3, 3, (1, 1), chanDim)
 		x = concatenate([conv_1x1, conv_3x3], axis=chanDim)
 
 		# return the block
@@ -29,7 +29,7 @@ class MiniGoogLeNet:
 	@staticmethod
 	def downsample_module(x, K, chanDim):
 		# define the CONV module and POOL, then concatenate across the channel dimensions
-		conv_3x3 = MiniGoogleNet.conv_module(x, K, 3, 3, (2, 2), chanDim, padding="valid")
+		conv_3x3 = MiniGoogLeNet.conv_module(x, K, 3, 3, (2, 2), chanDim, padding="valid")
 		pool = MaxPooling2D((3, 3), strides=(2, 2))(x)
 		x = concatenate([conv_3x3, pool], axis=chanDim)
 
@@ -49,23 +49,23 @@ class MiniGoogLeNet:
 
 		# define the model input and first CONV module
 		inputs = Input(shape=inputShape)
-		x = MiniGoogleNet.conv_module(inputs, 96, 3, 3, (1, 1), chanDim)
+		x = MiniGoogLeNet.conv_module(inputs, 96, 3, 3, (1, 1), chanDim)
 
 		# two Inception modules followed by a downsample module
-		x = MiniGoogleNet.inception_module(x, 32, 32, chanDim)
-		x = MiniGoogleNet.inception_module(x, 32, 48, chanDim)
-		x = MiniGoogleNet.downsample_module(x, 80, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 32, 32, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 32, 48, chanDim)
+		x = MiniGoogLeNet.downsample_module(x, 80, chanDim)
 
 		# four Inception modules followed by a downsample module
-		x = MiniGoogleNet.inception_module(x, 112, 48, chanDim)
-		x = MiniGoogleNet.inception_module(x, 96, 64, chanDim)
-		x = MiniGoogleNet.inception_module(x, 80, 80, chanDim)
-		x = MiniGoogleNet.inception_module(x, 48, 96, chanDim)
-		x = MiniGoogleNet.downsample_module(x, 96, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 112, 48, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 96, 64, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 80, 80, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 48, 96, chanDim)
+		x = MiniGoogLeNet.downsample_module(x, 96, chanDim)
 
 		# two Inception modules followed by a global POOL and dropout
-		x = MiniGoogleNet.inception_module(x, 176, 160, chanDim)
-		x = MiniGoogleNet.inception_module(x, 176, 160, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 176, 160, chanDim)
+		x = MiniGoogLeNet.inception_module(x, 176, 160, chanDim)
 		x = AveragePooling2D((7, 7))(x)
 		x = Dropout(0.5)(x)
 
